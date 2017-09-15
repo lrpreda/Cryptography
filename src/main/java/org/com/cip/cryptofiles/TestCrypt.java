de61fa7cadcceed3c9337024c6bda1f6a97f1e39
lrpre <lrpre@DESKTOP-NUV0UUM>
2017-09-15 16:25:53 -0300

    Changes

--- src/main/java/org/com/cip/cryptofiles/TestCrypt.java
+++ src/main/java/org/com/cip/cryptofiles/TestCrypt.java
@@ -14,29 +14,28 @@ import org.com.cip.cryptofiles.utils.LoadProperties;
  * @author lrpre
  */
 public class TestCrypt {
-    
-    public static void main(String args[]) throws Exception{
+
+    public static void main(String args[]) throws Exception {
         LoadProperties lProp;
         lProp = new LoadProperties("config.properties");
-        
+
         //Used to encrypt
         File senderPrivateKey = new File(lProp.getValue("senderPrivateKey"));
-        File receiverPublicKey= new File(lProp.getValue("receiverPublicKey"));
-        
+        File receiverPublicKey = new File(lProp.getValue("receiverPublicKey"));
+
         //Used to decrypt
         File receiverPrivateKey = new File(lProp.getValue("receiverPrivateKey"));
         File senderPublicKey = new File(lProp.getValue("senderPublicKey"));
-        
-        
-       File fileToBeEncrypted = new File("c://dev_tools//FILETO.ORIGINAL");
-       File fout = new File("c://dev_tools//FILEENCR.kry");
-       File fDec = new File ("c://dev_tools//FILE_DEC.txt");
-        
-       EncryptMain encry = new EncryptMain(lProp.getValue("sender"), lProp.getValue("receiver"), receiverPublicKey, senderPrivateKey, lProp.getValue("senderKeyPassword"));
-       encry.encrypt(new FileInputStream(fileToBeEncrypted), fout.toPath());
 
-       DecryptMain decry = new DecryptMain(senderPublicKey, receiverPrivateKey, lProp.getValue("receiverKeyPassword"));
-       decry.decrypt(new FileInputStream(fout),fDec.toPath());
+        File fileToBeEncrypted = new File("c://dev_tools//FILETO.ORIGINAL");
+        File fout = new File("c://dev_tools//FILEENCR.kry");
+        File fDec = new File("c://dev_tools//FILE_DEC.txt");
+
+        EncryptMain encry = new EncryptMain(lProp.getValue("sender"), lProp.getValue("receiver"), receiverPublicKey, senderPrivateKey, lProp.getValue("senderKeyPassword"));
+        encry.encrypt(new FileInputStream(fileToBeEncrypted), fout.toPath());
+
+        DecryptMain decry = new DecryptMain(senderPublicKey, receiverPrivateKey, lProp.getValue("receiverKeyPassword"));
+        decry.decrypt(new FileInputStream(fout), fDec.toPath());
     }
-    
+
 }
